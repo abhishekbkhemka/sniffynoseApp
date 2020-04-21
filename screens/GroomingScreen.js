ef0801936ed696121208c211694d253de1722d84
abhi <abhishekbkhemka@gmail.com>
2020-04-21 23:04:51 +0530

    otp verificartion

--- screens/GroomingScreen.js
+++ screens/GroomingScreen.js
@@ -1,7 +1,4 @@
 import * as React from 'react';
-import { View } from 'react-native';
-import { Card, CardItem, Text, Body } from "native-base";
-import styles from '../assets/styles/styles';
 
 import Grooming from '../components/Grooming';
 
